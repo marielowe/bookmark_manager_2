142e55ecf9e3cf29ddee94e4084a6599a22f9ea6
Daniel <daniel.szabo09@gmail.com>
2021-10-13 16:50:38 +0100

    Refactor index.erb

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ class BookmarkManager < Sinatra::Base
 
   get "/bookmarks" do
     @bookmarks = Bookmark.all_bookmarks
-    erb :index
+    erb :"bookmarks/index"
   end
 
   get "/bookmarks/new" do
